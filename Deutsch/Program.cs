6fb61e5fdd429ce8379453eb4b1eb8562904175b
OmegaRogue <thatomegarogue@gmail.com>
2019-11-17 18:56:06 +0100

    initial Commit

---
+++ Deutsch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FormalLang;
+
+namespace LangTest
+{
+	class Program
+	{
+		private static Language _lang;
+		static void Main(string[] args)
+		{
+			try
+			{
+				_lang = Language.Load(args[0]);
+			}
+			catch (Exception)
+			{
+				_lang = Language.Load("deutsch.json");
+			}
+			int  n;
+			bool log;
+			try
+			{
+				n = Convert.ToInt32(args[1]);
+			}
+			catch (Exception)
+			{
+				n = 1;
+			}
+
+			try
+			{
+				log = Convert.ToBoolean(args[2]);
+			}
+			catch (Exception)
+			{
+				log = false;
+			}
+            
+			for (var i = 0; i < n; i++)
+			{
+				Console.WriteLine(_lang.Replace(_lang.Startsymbol, log));
+			}
+		}
+	}
+}
---
+++ FormalLang/Language.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Newtonsoft.Json;
+
+namespace FormalLang
+{
+	public class Language
+	{
+		private static readonly Random Rng = new Random();
+
+		public string Startsymbol;
+		public char Trennzeichen;
+		public string[] Terminalsymbole;
+		public string[] Nichtterminalsymbole;
+		public Dictionary<string, string[]> Regeln;
+		
+		public Language(string                       startsymbol, char trennzeichen, string[] terminalsymbole, string[] nichtterminalsymbole,
+						Dictionary<string, string[]> regeln)
+		{
+			Startsymbol          = startsymbol;
+			Trennzeichen         = trennzeichen;
+			Terminalsymbole      = terminalsymbole;
+			Nichtterminalsymbole = nichtterminalsymbole;
+			Regeln               = regeln;
+		}
+		
+		public string Replace(string input, bool output = false)
+		{
+			if (output) Console.WriteLine(input);
+			while (true)
+			{
+				var stringarray = input.Split(Trennzeichen);
+				var buffer      = "";
+				var done        = false;
+				var wasntdone   = false;
+				foreach (var s in stringarray)
+				{
+					if (Terminalsymbole.Contains(s))
+					{
+						done   =  true;
+						buffer += s + Trennzeichen;
+					}
+
+					if (Nichtterminalsymbole.Contains(s))
+					{
+						done      = false;
+						wasntdone = true;
+					}
+
+					if (done) continue;
+					try
+					{
+						var arrBuffer = Regeln[s];
+						buffer += arrBuffer[Rng.Next(arrBuffer.Length)] + Trennzeichen;
+					}
+					catch (KeyNotFoundException)
+					{
+					}
+				}
+
+				if (output) Console.WriteLine(buffer);
+				if (done && !wasntdone) return buffer;
+				input = buffer;
+			}
+		}
+		
+		public static Language Load(string path)
+		{
+			var file = File.ReadAllText(path);
+			return JsonConvert.DeserializeObject<Language>(file);
+		}
+		public static void Write(Language lang, string path)
+		{
+			var file = File.CreateText(path);
+			file.Write(JsonConvert.SerializeObject(lang, Formatting.Indented));
+			file.Flush();
+			file.Close();
+		}
+
+		
+		
+	}
+}
